e82eff2338e9bb6e68eec305e88d5c2975b61027
tishex23 <bitcamp124@gmail.com>
2024-10-11 17:31:01 +0400

    finish About Section

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,7 +1,7 @@
 
 const Button = ({name, isBeam = false, containerClass}) => {
   return (
-    <button className={`btn $${containerClass}`}>
+    <button className={`btn ${containerClass}`}>
         {isBeam && (
             <span className="relative flex h-3 w-3">
                 <span className="btn-ping" />
---
+++ src/sections/About.jsx
@@ -0,0 +1,134 @@
+import Globe from "react-globe.gl"
+import Button from "../Components/Button"
+import { useState } from "react"
+
+const About = () => {
+
+    const [hasCopied, setHasCopied] = useState(false);
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText("lashamartiashvili.99@gmail.com");
+        setHasCopied(true);
+
+        setTimeout(() => {
+            setHasCopied(false);
+        }, 2000);
+    }
+
+  return (
+    <section className="c-space my-20">
+        <div className="grid xl:grid-cols-3 xl:grid-rows-6
+                   md:grid-cols-2 grid-cols-1 gap-5 h-full"
+        >
+            <div className="col-span-1 xl:row-span-3">
+                <div className="grid-container">
+                    <img src="assets/grid1.png" alt="grid-1"
+                     className="w-full sm:h-[276px] h-fit object-contain" 
+                     />
+
+                    <div>
+                        <p className="grid-headtext">
+                            Hi I am Tshex.
+                        </p>
+                        <p className="grid-subtext">
+                            I am a full-stack developer. I have 3 years of experience.
+                        </p>
+                    </div>
+
+                </div>
+                
+            </div>
+
+            <div className="col-span-1 xl:row-span-3">
+                <div className="grid-container">
+                    <img src="assets/grid2.png" alt="grid-2"
+                     className="w-full sm:h-[276px] h-fit object-contain" 
+                     />
+
+                    <div>
+                        <p className="grid-headtext">
+                            Tech Stack
+                        </p>
+                        <p className="grid-subtext">
+                            i specialize JavaScript/TypeScript with a strong focus on React.
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="col-span-1 xl:row-span-4">
+                <div className="grid-container">
+                    <div className="rounded-3x w-full sm:h-[326px] h-fit
+                        flex justify-center items-center
+                    ">
+                        <Globe 
+                            height={326}
+                            width={326}
+                            backgroundColor="rgba(0, 0, 0, 0)"
+                            backgroundImageOpacity={0.5}
+                            showAtmosphere
+                            showGraticules
+                            globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                            bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                        />
+                    </div>
+                    <div>
+                        <p className="grid-headtext">
+                          i work remotely across most   timezones.
+                        </p>
+                        <p className="grid-subtext">
+                            I'm based in Georgia, wth remote work avaliable.
+                        </p>
+                        <Button name="Contact me" isBeam containerClass="w-full mt-10" />
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className="xl:col-span-2 xl:row-span-3">
+                <div className="grid-container">
+                    <img src="assets/grid3.png" alt="grid-3"
+                     className="w-full sm:h-[266px] h-fit object-contain" 
+                     />
+                     <div>
+                        <p className="grid-headtext">
+                            My Passion for Coding
+                        </p>
+                        <p className="grid-subtext">
+                            I am a full-stack developer. I have 3 years of experience.
+                        </p>
+                     </div>
+                </div>
+            </div>
+
+            <div className="xl:col-span-1 xl:row-span-2">
+                <div className="grid-container">
+                    <img src="assets/grid4.png" alt="grid-4"
+                     className="w-full md:h-[126px] sm:h-[276px] 
+                     h-fit object-cover sm:object-top" 
+                     />
+                     <div className="space-y-2">
+                        <p className="grid-headtext text-center">
+                            Contact me
+                        </p>
+                       <div className="copy-container" onClick={handleCopy}>
+                            <img src={hasCopied ? "assets/tick.svg" 
+                                                : "assets/copy.svg"} />
+                            <p className="lg:text-xl md:text-lg font-medium 
+                            text-gray_gradient text-white">
+                                lashamartiashvili.99@gmail.com
+                            </p>
+                       </div>
+                     </div>
+
+                </div>
+            </div>
+
+        </div>
+    </section>
+  )
+}
+
+export default About
